0f98552e6a467ba73a208a3d5eca3892eb4c8623
Canas <crsilva@ing.uchile.cl>
2017-01-23 18:55:17 -0300

    Added simple KRLS implementation.

--- kernel_filtering/filters.py
+++ kernel_filtering/filters.py
@@ -1,6 +1,52 @@
 import numpy as np
 
 
+def simple_krls(x, y, kernel, regularizer=1.0):
+    """ Simple KRLS for 1-channel data.
+
+    :param x: input array
+    :param y: target array
+    :param kernel: kernel object from Kernel class
+    :param regularizer: lambda parameter for regularization
+    :return: estimations, coefficients and error history
+    """
+    n = x.shape[0]
+
+    q = np.array((regularizer + kernel(x[0], x[0]))**(-1))
+    a_coef = np.array([q * y[0]])
+
+    estimate = np.zeros(y.shape)
+    support_vectors = [x[0]]
+    error_history = [0]
+
+    for i in range(1, n):
+        regressor = x[i]
+
+        h = kernel(support_vectors, regressor).T
+        z = np.dot(q, h)
+        r = regularizer + kernel(x[i], x[i]) - np.dot(z, h)
+
+        q = q * r + np.outer(z, z.T)
+        q_row = np.asarray(-z).reshape(-1, 1).T
+        q_col = np.asarray(-z).reshape(-1, 1)
+        q_end = np.array([1]).reshape(-1, 1)
+
+        q = np.append(q, q_row, axis=0)
+        q = np.append(q, np.concatenate((q_col, q_end), axis=0), axis=1)
+        q = r**(-1) * q
+
+        estimate[i] = np.dot(h.T, a_coef)
+        error = y[i] - estimate[i]
+
+        a_coef -= z * r**(-1) * error
+        a_coef = np.append(a_coef, r**(-1) * error)
+
+        support_vectors.append(x[i])
+        error_history.append(error)
+
+    return estimate, a_coef, error_history
+
+
 def klmsx(x, y, kernel, learning_rate=0.1, sparsify=None, delay=0):
     """ Implementation of multi-channel klms.
 
@@ -14,6 +60,7 @@ def klmsx(x, y, kernel, learning_rate=0.1, sparsify=None, delay=0):
     """
 
     assert delay <= len(x)
+    assert len(kernel.params) == x.shape[-1]
 
     n = x.shape[0]
 
@@ -46,7 +93,7 @@ def klmsx(x, y, kernel, learning_rate=0.1, sparsify=None, delay=0):
     return estimate, a_coef, error_history
 
 
-def simple_klms(x, y, kernel, learning_rate=0.1, sparsify=None):
+def simple_klms(x, y, kernel, learning_rate=0.1):
     """ Simple KLMS for 1-channel data
 
     :param x: input array
@@ -73,7 +120,7 @@ def simple_klms(x, y, kernel, learning_rate=0.1, sparsify=None):
     return f, a
 
 
-def delayed_klms(x, y, kernel, learning_rate=0.1, sparsify=None, delay=0):
+def delayed_klms(x, y, kernel, learning_rate=0.1, delay=0):
     """ Simple KLMS for 1-channel data and a fixed delay
 
     :param x:
