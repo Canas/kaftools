121c35f99617a0c56d8a33056f1fb15549a6c2ac
Canas <crsilva@ing.uchile.cl>
2017-05-18 17:40:00 -0400

    Modify setup.py for PyPi package upload

--- setup.py
+++ setup.py
@@ -1,17 +1,34 @@
+import os
+from codecs import open
 from setuptools import setup, find_packages
+
+
+# Get long description from README
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='kaftools',
-    version='0.1',
-    packages=find_packages(),
-    url='',
-    license='MIT',
+    version='0.1.0',
+    description='Small extensible package for Kernel Adaptive Filtering (KAF) methods.',
+    long_description=long_description,
+    url='https://github.com/canas/kaftools',
     author='Cristóbal Silva',
     author_email='crsilva@ing.uchile.cl',
-    description='Small extensible package for Kernel Adaptive Filtering (KAF) methods.',
+    license='MIT',
+    packages=find_packages(),
     install_requires=[
         'numpy',
         'matplotlib',
         'scipy',
-    ]
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords='kernel adaptive filters kaf'
 )
